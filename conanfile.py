8d483288acd9c4d6f617ec1f8f4626a8f5d02f9e
Fabian Poggenhans <fabian.poggenhans@kit.edu>
2019-08-05 18:43:42 +0200

    Add support for lanelet2_python in conan

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import em
+import xml.etree.ElementTree as ET
 from conans import ConanFile, CMake, tools
 
 find_mrt_cmake="""
@@ -8,10 +9,38 @@ set(mrt_cmake_modules_FOUND True)
 include(${CMAKE_CURRENT_LIST_DIR}/mrt_cmake_modules-extras.cmake)
 """
 
+cmake_lists="""
+cmake_minimum_required(VERSION 3.0)
+project(lanelet2)
+set(CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+set(BoostPython_FOUND Yes)
+include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
+conan_basic_setup(SKIP_STD)
+
+# declare dependencies
+set(lanelet2_io_LIBRARIES lanelet2_core)
+set(lanelet2_projection_LIBRARIES lanelet2_core)
+set(lanelet2_traffic_rules_LIBRARIES lanelet2_core)
+set(lanelet2_routing_LIBRARIES lanelet2_core lanelet2_traffic_rules)
+set(lanelet2_validation_LIBRARIES lanelet2_core lanelet2_io lanelet2_routing lanelet2_traffic_rules lanelet2_projection)
+set(lanelet2_examples_LIBRARIES lanelet2_core lanelet2_io lanelet2_routing lanelet2_traffic_rules lanelet2_projection)
+set(lanelet2_python_LIBRARIES lanelet2_core lanelet2_io lanelet2_routing lanelet2_traffic_rules lanelet2_projection)
+include_directories(lanelet2_core/include lanelet2_io/include lanelet2_projection/include lanelet2_traffic_rules/include
+    lanelet2_routing/include lanelet2_validation/include)
+add_subdirectory(lanelet2_core)
+add_subdirectory(lanelet2_io)
+add_subdirectory(lanelet2_projection)
+add_subdirectory(lanelet2_traffic_rules)
+add_subdirectory(lanelet2_routing)
+add_subdirectory(lanelet2_validation)
+add_subdirectory(lanelet2_examples)
+add_subdirectory(lanelet2_python)
+add_subdirectory(lanelet2_maps)
+"""
+
 def read_version():
-    with open('version.txt') as f:
-        line = next(f)
-    return line.rstrip()
+    package = ET.parse('lanelet2_core/package.xml')
+    return package.find('version').text
 
 def get_py_version():
     return "{}.{}".format(sys.version_info.major, sys.version_info.minor)
@@ -24,27 +53,17 @@ class Lanelet2Conan(ConanFile):
     license = "BSD"
     url = "https://github.com/fzi-forschungszentrum-informatik/lanelet2"
     description = "Map handling framework for automated driving"
-    options = {"shared": [True, False]}
-    default_options = {"shared": False} #, "boost_python:python_version": get_py_version()}
+    options = {"shared": [True, False], "fPIC": [True]}
+    default_options = {"shared": False, "fPIC": True, "boost:python_version": get_py_version(), "boost:without_python": False}
 
-    requires = ("boost/1.64.0@conan/stable",
-                # "boost_serialization/1.68.0@bincrafters/stable",
-                # "boost_geometry/1.68.0@bincrafters/stable",
-                # "boost_optional/1.68.0@bincrafters/stable",
-                # "boost_variant/1.68.0@bincrafters/stable",
-                # "boost_units/1.68.0@bincrafters/stable",
-                # "boost_lexical_cast/1.68.0@bincrafters/stable",
-                # "boost_filesystem/1.68.0@bincrafters/stable",
-                # "boost_property_map/1.68.0@bincrafters/stable",
-                # "boost_iterator/1.68.0@bincrafters/stable",
-                # "boost_program_options/1.68.0@bincrafters/stable",
-                # "boost_python/1.68.0@bincrafters/stable",
+    requires = ("python_dev_config/0.6@bincrafters/stable",
+                "boost/1.69.0@conan/stable",
                 "eigen/3.3.7@conan/stable",
                 "geographiclib/1.49@bincrafters/stable",
                 "pugixml/1.9@bincrafters/stable")
 
     exports_sources = "*"
-    exports = "version.txt"
+    exports = "lanelet2_core/package.xml"
 
     proj_list = [
         'lanelet2_core',
@@ -61,8 +80,12 @@ class Lanelet2Conan(ConanFile):
         cmake.configure()
         return cmake
 
+    def _pythonpath(self):
+        return os.path.join("lib", "python" + get_py_version(), "site-packages")
+
     def source(self):
-        self.run("git clone https://github.com/KIT-MRT/mrt_cmake_modules.git --branch conan_support")
+        if not os.path.exists("mrt_cmake_modules"):
+            self.run("git clone https://github.com/KIT-MRT/mrt_cmake_modules.git --branch conan_support")
         mrt_cmake_dir = os.path.join(os.getcwd(), "mrt_cmake_modules")
         with open("mrt_cmake_modules/cmake/mrt_cmake_modules-extras.cmake.em") as f:
             extras = em.expand(f.read(), DEVELSPACE=True, PROJECT_SOURCE_DIR=mrt_cmake_dir,
@@ -71,6 +94,8 @@ class Lanelet2Conan(ConanFile):
             f.write(extras)
         with open("Findmrt_cmake_modules.cmake", "w") as f:
             f.write(find_mrt_cmake)
+        with open("CMakeLists.txt", "w") as f:
+            f.write(cmake_lists)
 
     def build(self):
         cmake = self._configure_cmake()
@@ -82,3 +107,4 @@ class Lanelet2Conan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = list(reversed(self.proj_list))
+        self.env_info.PYTHONPATH.append(os.path.join(self.package_folder, self._pythonpath()))
